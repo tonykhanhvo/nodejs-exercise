cd373a212b51a8995be6ce1e0c3ce62b9141b8df
tonykhanhvo <vgkhanh1997@gmail.com>
2022-02-25 16:58:04 +0700

    Adding Middleware

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('In the middleware here!');
+  next(); //Call next() cho phep request di den middleware tiep theo
+});
+
+app.use((req, res, next) => {
+  console.log('In another middleware!')
+  //... Send Response
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
