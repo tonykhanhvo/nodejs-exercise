83033811dd88383acc548b1f97838485e2c6ef4a
tonykhanhvo <vgkhanh1997@gmail.com>
2022-02-25 17:23:42 +0700

    Send response with Middleware

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   console.log('In another middleware!')
-  //... Send Response
+  res.send('<h1>Hello from Express!</h1>')
 });
 
 const server = http.createServer(app);
