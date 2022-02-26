61bba52a137deaf2a05c3f062e81149a16fdbfa6
tonykhanhvo <vgkhanh1997@gmail.com>
2022-02-26 22:54:22 +0700

    Parse Incomming Request to take body request

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const http = require('http');
 
 const express = require('express');
+const bodyParse = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-  console.log('This always run!');
-  next();
-});
+app.use(bodyParse.urlencoded({extended: false}));
 
 app.use('/add-product', (req, res, next) => {
-  console.log('Middleware add-product');
-  res.send('<h1>The "Add Product" Page</h1>');
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-  console.log('Middleware slash');
   res.send('<h1>Hello from Express!</h1>');
 });
 
