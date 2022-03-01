b7d076911c2a97be4a94277ae18f5c0e6d8cd5b6
tonykhanhvo <vgkhanh1997@gmail.com>
2022-03-01 16:47:24 +0700

    Serving Statically File

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParse.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
