18a6f2b6f83156eb78f62eb6ac1147f2846b0b10
tonykhanhvo <vgkhanh1997@gmail.com>
2022-03-04 21:52:53 +0700

    Copy source code from Udemy

--- models/product.js
+++ models/product.js
@@ -1,39 +1,40 @@
 const fs = require('fs');
 const path = require('path');
 
-const rootDir = require('../util/path');
 const p = path.join(
-  rootDir,
+  path.dirname(process.mainModule.filename),
   'data',
   'products.json'
 );
 
-//Create a Helper Function
 const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
-      return cb([]);
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
-    return cb(JSON.parse(fileContent));
   });
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
     getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
+      fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
     });
-    
   }
 
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
-}
+};
