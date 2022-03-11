24af068524738cf15d670a935ac6ca8c8391699f
tonykhanhvo <vgkhanh1997@gmail.com>
2022-03-11 20:03:36 +0700

    Updating Product

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,5 @@
+const { escapeRegExpChars } = require('ejs/lib/utils');
+const { RESERVED } = require('mysql2/lib/constants/client');
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
@@ -34,17 +36,19 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    if (!product) {
-      return res.redirect('/');
-    }
-    res.render('admin/edit-product', {
-      pageTitle: 'Add Product',
-      path: '/admin/edit-product',
-      editing: editMode,
-      product: product
-  })
-  });
+  Product.findById(prodId)
+    .then(product => {
+      if (!product) {
+        return res.redirect('/');
+      }
+      res.render('admin/edit-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/edit-product',
+        editing: editMode,
+        product: product
+      });
+    })
+    .catch(err => {console.log(err)});
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -53,9 +57,19 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct =  new Product(prodId, updatedTitle, updatedImageUrl, updatedDesc, updatedPrice);
-  updatedProduct.save();
-  res.redirect('/admin/products');
+  Product.findById(prodId)
+    .then(product => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.imageUrl = updatedImageUrl;
+      product.description = updatedDesc;
+      return product.save();
+    })
+    .then(result => {
+      console.log('UPDATED PRODUCT!');
+      res.redirect('/admin/products');
+    })
+    .catch(err => {console.log(err)});
 }
 
 exports.getProducts = (req, res, next) => {
