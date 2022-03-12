0a5b657d58bc8b385af0285a332d699fedcbcbe2
tonykhanhvo <vgkhanh1997@gmail.com>
2022-03-12 12:51:56 +0700

    Createing & Managing a Dummy User

--- app.js
+++ app.js
@@ -19,6 +19,17 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParse.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findById(1)
+    .then(user => {
+      req.user = user; //them 1 field moi de luu sequelize object (user) vao request
+      next();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -30,7 +41,17 @@ Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
 sequelize
   .sync({force: true})
   .then(result => {
+    return User.findById(1);
     // console.log('🚀 ~ file: app.js ~ line 26 ~ sequelize.sync ~ result', result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({name: 'Max', email: 'test@test.com'});
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
     app.listen(3000);
   })
   .catch(err => {
