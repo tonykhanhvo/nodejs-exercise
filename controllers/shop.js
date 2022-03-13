e196f7bdb6f86a9a0a268f93de9cd239f841c6b5
tonykhanhvo <vgkhanh1997@gmail.com>
2022-03-13 16:07:10 +0700

    Resetting the Cart & Fetching and Outputting Orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -142,8 +142,10 @@ exports.postCartDeleteProduct = (req, res, next) => {
 }
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user.getCart()
     .then(cart => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then(products => {
@@ -157,21 +159,23 @@ exports.postOrder = (req, res, next) => {
         .catch(err => {console.log(err)});
     })
     .then(result => {
+      return fetchedCart.setProducts(null);
+    })
+    .then(result => {
       res.redirect('/orders');
     })
     .catch(err => console.log(err));
 }
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
-};
-
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout'
-  });
+  req.user.getOrders({include: ['products']})
+    .then(orders => {
+      console.log(orders);
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders
+      });
+    })
+    .catch(err => {console.log(err)});
 };
