c5c4c6d5b77fb640bda72f714ac2ac40d078a6c5
tonykhanhvo <vgkhanh1997@gmail.com>
2022-03-14 22:15:36 +0700

    Creating Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,13 +15,8 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user
-    .createProduct({
-      title: title,
-      price: price,
-      imageUrl: imageUrl,
-      description: description
-    })
+  const product = new Product(title, price, description, imageUrl);
+  product.save()
     .then(result => {
       // console.log('🚀 ~ file: admin.js ~ line 23 ~ result', result);
       console.log('Created Product');
