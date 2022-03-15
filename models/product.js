9071ff7a1fc55f9b2aff391b939ea3efcbf68d3f
tonykhanhvo <vgkhanh1997@gmail.com>
2022-03-15 20:04:40 +0700

    Working on Product Model to Edit Our Product

--- models/product.js
+++ models/product.js
@@ -2,17 +2,25 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
-    return db.collection('products')
-      .insertOne(this)
+    let dbOp;
+    if (this._id) {
+      //Update the Product
+      dbOp = db.collection('products').updateOne({_id: new mongodb.ObjectId(this._id)}, {$set: this});
+    } else {
+      //Insert new Product
+      dbOp = db.collection('products').insertOne(this);
+    }
+    return dbOp
       .then(result => {
         console.log(result);
       })
