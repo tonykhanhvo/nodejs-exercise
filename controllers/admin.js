fb2ac45f1f1516dbed88ac0355ed14b3fb02e069
tonykhanhvo <vgkhanh1997@gmail.com>
2022-03-15 20:52:42 +0700

    Delete Product

--- controllers/admin.js
+++ controllers/admin.js
@@ -82,15 +82,12 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-// exports.postDeleteProduct = (req, res, next) => {
-//   const prodId = req.body.productId;
-//   Product.findById(prodId)
-//     .then(product => {
-//       return product.destroy();
-//     })
-//     .then(result => {
-//       console.log('DELETING PRODUCT');
-//       res.redirect('/admin/products');
-//     })
-//     .catch(err => {console.log(err)});
-// };
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.deleteById(prodId)
+    .then(() => {
+      console.log('DELETING PRODUCT');
+      res.redirect('/admin/products');
+    })
+    .catch(err => {console.log(err)});
+};
--- models/product.js
+++ models/product.js
@@ -50,6 +50,16 @@ class Product {
         console.log(err)
       });
   }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db.collection('products')
+      .deleteOne({_id: new mongodb.ObjectId(prodId)})
+      .then(result => {
+        console.log('DELETED !!!')
+      })
+      .catch(err => console.log(err));
+  }
 }
 
 module.exports = Product
