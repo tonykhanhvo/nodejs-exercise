100e7a790bdb7cf1e55f65e48148a78b3938b2c4
tonykhanhvo <vgkhanh1997@gmail.com>
2022-03-16 23:39:56 +0700

    Saving Data Through Mongoose

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParse = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -18,23 +18,23 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParse.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('6230a2326f8df3d77c2ba0ac')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id); //ntn se nhan day du cac method tu class User
-      next();
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
+// app.use((req, res, next) => {
+//   User.findById('6230a2326f8df3d77c2ba0ac')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id); //ntn se nhan day du cac method tu class User
+//       next();
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     });
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://funix-nodejs:funix-node@cluster0.kw5wd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://funix-nodejs:funix-nodejs@cluster0.kw5wd.mongodb.net/shop?retryWrites=true&w=majority')
   .then(result => {
     app.listen(3000);
     console.log('CONNECTED !!!')
--- controllers/admin.js
+++ controllers/admin.js
@@ -13,14 +13,12 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(
-    title,
-    price,
-    description,
-    imageUrl,
-    null,
-    req.user._id //STRING type value
-  );
+  const product = new Product({
+    title: title,
+    price: price,
+    description: description,
+    imageUrl: imageUrl
+  });
   product.save()
     .then(result => {
       // console.log('🚀 ~ file: admin.js ~ line 23 ~ result', result);
