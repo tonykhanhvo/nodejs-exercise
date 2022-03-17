7ea29cf0fb7bdab3ac47fc15246c5be5b8732167
tonykhanhvo <vgkhanh1997@gmail.com>
2022-03-17 21:32:52 +0700

    Working on the Shopping Cart

--- models/user.js
+++ models/user.js
@@ -26,6 +26,29 @@ const userSchema = new Schema({
   }
 });
 
+userSchema.methods.addToCart = function(product) {
+  const cartProductIndex = this.cart.items.findIndex(cp => {
+    return cp.prodId.toString() === product._id.toString(); // Vi _id khong phai kieu STRING ma la ObjectId
+  });
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
+
+  if (cartProductIndex >= 0) {
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  } else {
+    updatedCartItems.push({
+      prodId: product._id,
+      quantity: newQuantity
+    });
+  }
+  const updatedCart = {
+    items: updatedCartItems
+  };
+  this.cart = updatedCart;
+  return this.save();
+}
+
 module.exports = mongoose.model('User', userSchema)
 
 
