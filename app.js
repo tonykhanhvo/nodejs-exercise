a1afaea6fe7fe18d184a5eec7c169b9a0ab9b0cb
tonykhanhvo <vgkhanh1997@gmail.com>
2022-03-17 17:00:32 +0700

    Adding & Using User Model

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParse = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,16 +18,16 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParse.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('6230a2326f8df3d77c2ba0ac')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id); //ntn se nhan day du cac method tu class User
-//       next();
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     });
-// });
+app.use((req, res, next) => {
+  User.findById('62330680cb57770ee440f8d9')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -36,6 +36,20 @@ app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://funix-nodejs:funix-nodejs@cluster0.kw5wd.mongodb.net/shop?retryWrites=true&w=majority')
   .then(result => {
+    User.findOne()
+      .then(user => {
+        if (!user) {
+          const user = new User({
+            name: 'Max',
+            email: 'tonyvo@test.com',
+            cart: {
+              items: []
+            }
+          });
+          user.save();
+          console.log('Created User');
+        }
+      })
     app.listen(3000);
     console.log('CONNECTED !!!')
   })
