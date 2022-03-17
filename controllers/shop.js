c6b37a371c4881ae695c009fbfa024407d6c166e
tonykhanhvo <vgkhanh1997@gmail.com>
2022-03-17 21:57:18 +0700

    Loadding the Cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -47,8 +47,11 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {
+    .populate('cart.items.prodId')
+    .execPopulate()
+    .then(user => {
+      console.log(user.cart.items);
+      const products = user.cart.items;
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
