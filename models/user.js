65220099a3b1cf5b9fad1aa038d983a716731ffe
tonykhanhvo <vgkhanh1997@gmail.com>
2022-03-18 12:29:18 +0700

    Clearing the Cart After Storing an Order

--- models/user.js
+++ models/user.js
@@ -57,6 +57,11 @@ userSchema.methods.removeFromCart = function(prodId) {
   return this.save();
 }
 
+userSchema.methods.clearCart = function() {
+  this.cart = {items: []};
+  return this.save();
+}
+
 module.exports = mongoose.model('User', userSchema)
 
 
