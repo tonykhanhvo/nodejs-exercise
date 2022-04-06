9438a66ce068336940391b30bd5596fca1f4a879
tonykhanhvo <vgkhanh1997@gmail.com>
2022-04-06 19:16:29 +0700

    Set up & Create Login Form

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParse.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -31,6 +32,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
