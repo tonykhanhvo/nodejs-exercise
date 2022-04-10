faa03688448aa8bbf6d2a72f258747f77380442d
tonykhanhvo <vgkhanh1997@gmail.com>
2022-04-10 22:34:48 +0700

    Deleting Cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
   const isLoggedIn = req.get('Cookie') ? req
     .get('Cookie')
@@ -10,6 +12,18 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  req.session.isLoggedIn = true;
-  res.redirect('/');
-}
+  User.findById('62330680cb57770ee440f8d9')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
+};
