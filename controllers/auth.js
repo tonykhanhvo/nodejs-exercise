620e95cdf1bb011c7ec39531c8ccfa44ce821d32
tonykhanhvo <vgkhanh1997@gmail.com>
2022-05-07 23:15:30 +0700

    Using Validation Error Messages

--- controllers/auth.js
+++ controllers/auth.js
@@ -80,7 +80,7 @@ exports.postSignup = (req, res, next) => {
       path: '/signup',
       pageTitle: 'Signup',
       isAuthenticated: false,
-      errorMessage: errors.array()
+      errorMessage: errors.array()[0].msg
     });
   }
   User.findOne({email: email})
